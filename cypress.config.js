2fa25aa2641099ddbe80b90070faf2d439537542
pippopil <voood234@gmail.com>
2023-01-30 17:39:06 +0300

    Add files

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+    retries: 1,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
